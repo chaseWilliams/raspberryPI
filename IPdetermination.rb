74f623fc1c39ab19c378714ef3f4a996f8467726
Chase Williams <bornofawesomeness@gmail.com>
2015-12-29 21:34:21 -0500

    improved ip address algorithm

--- IPdetermination.rb
+++ IPdetermination.rb
@@ -3,9 +3,9 @@
 #spawn "echo #{payload}" => stuff
 
 require 'rest-client'
-address = Socket.getaddrinfo(Socket.gethostname, nil)[0][2]
+address = Socket.ip_address_list.detect {|x| x.ipv4_private?}.ip_address
 name = Socket.gethostname
 if name.include? '.' then name = name.slice(0..name.index('.') - 1) end
-#WARNING 'grep' line needs to be changed to '10.10.10' for WGSOHO
-payload = {:"text" => "The device #{name}'s IP Address is #{address}"}.to_json
+payload = {text: "*Device:* `#{name}`\n *IP:* `#{address}`"}.to_json
 RestClient.post 'https://hooks.slack.com/services/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', payload
+puts 'successfully posted to Slack.'
