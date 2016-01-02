12921b755da394ea765a455132f22eebebc88ae5
chaseWilliams <bornofawesomeness@gmail.com>
2016-01-01 23:36:18 -0500

    improved IPdetermination.rb

--- IPdetermination.rb
+++ IPdetermination.rb
@@ -1,11 +1,8 @@
-#example command line code
-#payload = 'stuff'
-#spawn "echo #{payload}" => stuff
-
+#sends a message to slack using incoming-webhook that identifies that 
+#host machine's name and ip address.
 require 'rest-client'
 address = Socket.ip_address_list.detect {|x| x.ipv4_private?}.ip_address
 name = Socket.gethostname
 if name.include? '.' then name = name.slice(0..name.index('.') - 1) end
 payload = {text: "*Device:* `#{name}`\n *IP:* `#{address}`"}.to_json
 RestClient.post 'https://hooks.slack.com/services/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', payload
-puts 'successfully posted to Slack.'
