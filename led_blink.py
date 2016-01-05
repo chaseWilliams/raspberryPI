a9d0f215f6f7bd0297dfe863090c7004ec03e832
chaseWilliams <bornofawesomeness@gmail.com>
2016-01-05 16:58:53 -0500

    More alterations to the led_blink.py file

--- led_blink.py
+++ led_blink.py
@@ -2,20 +2,18 @@ import RPi.GPIO as GPIO
 import time, random
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
 blue_led = 12
 red_led = 17
 green_led = 13
-button = 18
+button = 20
 
 GPIO.setup(blue_led, GPIO.OUT)
 GPIO.setup(red_led, GPIO.OUT)
 GPIO.setup(green_led, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 while True:
-    if !(GPIO.input(button)):
+    if GPIO.input(button) == 0:
         x = 2
         print("Enjoy!")
         while x > 0:
@@ -31,5 +29,4 @@ while True:
             x = x - 1
         print("Done.")
         break
-
 GPIO.cleanup()
