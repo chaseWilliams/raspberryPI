1ab0bd3a163b7ec9b916f0d6ab667dd6896675fb
chaseWilliams <bornofawesomeness@gmail.com>
2016-01-20 21:19:11 -0500

    fixed margin whitespace, created the gradient rectangle. Now need measuring triangle and additional data: wind, cloud, and humidity. possible google calendar api integration?

--- weatherStation/app/controllers/pages_controller.rb
+++ weatherStation/app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
   def home
     `sudo python /home/pi/Documents/coding/raspberryPI/weatherStation/app/led_blink.py`
     server = WeatherMan.new
-    $outside_data = server.getWeather(4219934)
-    $sensor_temp = DRead.read_data('/home/pi/Documents/coding/raspberryPI/weatherStation/app/data.txt', 'temperature')
-    $sensor_temp = ($sensor_temp.to_f * (9.0/5) + 32).round(2)
+    @outside_data = server.getWeather(4219934)
+    @sensor_temp = DRead.read_data('/home/pi/Documents/coding/raspberryPI/weatherStation/app/data.txt', 'temperature')
+    @sensor_temp = (@sensor_temp.to_f * (9.0/5) + 32).round(2)
   end
 end
