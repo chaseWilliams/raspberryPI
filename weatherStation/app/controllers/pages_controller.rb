34c1ecd8d4e2156a038be92c8f1a110ed2d094f1
chaseWilliams <bornofawesomeness@gmail.com>
2016-01-22 19:41:23 -0500

    new style

--- weatherStation/app/controllers/pages_controller.rb
+++ weatherStation/app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
-load '/home/pi/Documents/coding/raspberryPI/weatherStation/app/data_reader.rb'
-load '/home/pi/Documents/coding/raspberryPI/weatherStation/app/weatherstation.rb'
+load '~/Documents/coding/raspberryPI/weatherStation/app/data_reader.rb'
+load '~/Documents/coding/raspberryPI/weatherStation/app/weatherstation.rb'
 class PagesController < ApplicationController
   def home
     `sudo python /home/pi/Documents/coding/raspberryPI/weatherStation/app/led_blink.py`
     server = WeatherMan.new
     @outside_data = server.getWeather(4219934)
-    @sensor_temp = DRead.read_data('/home/pi/Documents/coding/raspberryPI/weatherStation/app/data.txt', 'temperature')
-    @sensor_temp = (@sensor_temp.to_f * (9.0/5) + 32).round(2)
+   # @sensor_temp = DRead.read_data('/home/pi/Documents/coding/raspberryPI/weatherStation/app/data.txt', 'temperature')
+    #@sensor_temp = (@sensor_temp.to_f * (9.0/5) + 32).round(2)
   end
 end
