f67b5223d2fb34ceba4b58b6303bf91c9b2fe72c
Chase Williams <bornofawesomeness@gmail.com>
2016-01-29 15:24:11 +0000

    headway on flickr module

--- weatherStation/app/flickr_rand_pic.rb
+++ weatherStation/app/flickr_rand_pic.rb
@@ -4,4 +4,4 @@ require 'flickr.rb'
 module Flickr
     def imgUrl photo_id
         response = JSON.parse RestClient.get "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3a86e2e6e0552b135fa3830f8421d07e&format=json&photo_id=#{photo_id}"
-        
+        return response[:sizes][:size][8][:source]
